481b66b32891f1616ce88ebc680a96d467c52a30
Owen Wang <owang@twitter.com>
2014-04-02 18:54:29 -0700

    more ui

--- static/js/main.js
+++ static/js/main.js
@@ -6,28 +6,52 @@ $(function() {
 			$numAtTable = $('#num-at-table'),
 			$numPosition = $('#position');
 
+	var updateNums = function() {
+		$numAtTable.html(numAtTable);
+		$numPosition.html(numPosition);
+		console.log(numAtTable);
+		console.log(numPosition);
+	};
+
 	$('#start').click(function(e) {
 		numAtTable = $('#table-num').val();
 		numPosition = $('#position-num').val();
 
-		$numAtTable.html(numAtTable);
-		$numPosition.html(numPosition);
+		updateNums();
 
 		$('#start-wrapper').hide();
 		$('#game-wrapper').show();
 	});
 
 	$('.counter').click(function(e) {
+		var $target = $(e.target);
+
+		if ($target.hasClass('up')) {
+			if ($target.hasClass('ppl')) {
+				numAtTable++;
+			} else {
+				numPosition++;
+			}
+		} else {
+			if ($target.hasClass('ppl')) {
+				numAtTable--;
+			} else {
+				numPosition--;
+			}
+		}
+
+		updateNums();
 	});
 
 	//fb.push({action: "derp"});
-//happens when something new gets added
-fb.on('child_added', function(snapshot) {
-	console.log(snapshot.val());
-});
+	
+	//happens when something new gets added
+	fb.on('child_added', function(snapshot) {
+		console.log(snapshot.val());
+	});
 
-//happens when page loads. populate stats data 
-fb.on('value', function(snapshot) {
-	console.log(snapshot);
-});
+	//happens when page loads. populate stats data 
+	fb.on('value', function(snapshot) {
+		console.log(snapshot);
+	});
 });
